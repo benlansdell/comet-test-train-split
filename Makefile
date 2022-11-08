3ac051a4f1787365ac630d87c5750146e60c260e
benlansdell <ben.lansdell@stjude.org>
2022-11-08 16:38:08 -0600

    Add test/validation/test split

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ ingress:
 convert:
 		python src/convert_xls_csv.py data/input/COMET_DATA_010322.xlsx data/processed/comet_metadata.csv
 split:
-		python src/testtrainsplit.py data/processed/comet_metadata.csv data/output/comet_metadata_split.csv
+		python src/testtrainsplit.py data/processed/comet_metadata.csv data/output/comet_metadata_split.csv --test_size 0.5 --val_size 0.0
+
+valsplit:
+		python src/testtrainsplit.py data/processed/comet_metadata.csv data/output/comet_metadata_trainvalsplit.csv
 
 all: ingress split
--- src/testtrainsplit.py
+++ src/testtrainsplit.py
@@ -8,7 +8,8 @@ SEED = 42
 parser = argparse.ArgumentParser(description='Assign slides to test or train group')
 parser.add_argument('input', help='CSV file to compute test train split for')
 parser.add_argument('output', help='Output csv file with test train assignments')
-parser.add_argument('--test_size', type=float, default=0.5, help='Fraction of data to assign to test group')
+parser.add_argument('--test_size', type=float, default=0.1, help='Fraction of data to assign to test group')
+parser.add_argument('--val_size', type=float, default=0.1, help='Fraction of data to assign to val group')
 
 args = parser.parse_args(['../data/processed/comet_metadata.csv', '../data/output/comet_metadata_split.csv'])
 
@@ -26,11 +27,25 @@ def main(args):
         df['Slide Scan File'] = df['Slide Scan File'].apply(lambda x: str(int(x)) if not pd.isna(x) else x)
     else:
         df['disease_sheet_name'] = df['sheet name'].apply(lambda x: x.split(' ')[0])
+    df['val'] = False
     df['test'] = False
 
     splitter = StratifiedShuffleSplit(n_splits=1, test_size=args.test_size, random_state=SEED)
     for train_index, test_index in splitter.split(df, df['disease_sheet_name']):
-        df.iloc[test_index.astype(int), -1] = True
+        train_data = df.iloc[train_index]
+        test_data = df.iloc[test_index]
+        test_data['test'] = True
+        #df.iloc[test_index.astype(int), -1] = True
+        if args.val_size > 0:
+            val_splitter = StratifiedShuffleSplit(n_splits=1, test_size=args.val_size/(1-args.test_size), random_state=SEED)
+            for train_index_2, val_index in val_splitter.split(train_data, train_data['disease_sheet_name']):
+                val_data = train_data.iloc[val_index]
+                train_data = train_data.iloc[train_index_2]
+                val_data['val'] = True
+                df = pd.concat([train_data, val_data, test_data])
+        else:
+            test_data['val'] = True
+            df = pd.concat([train_data, test_data])
 
     os.makedirs(os.path.dirname(args.output), exist_ok=True)
     df.to_csv(args.output, index=False)
